8c73c66e7f06d9490ea7b61cd4eeb09bd519ff02
gk0909c <gk0909c@gmail.com>
2017-01-20 17:40:34 +0900

    connect salesforce from local proxy, it's ok for now.

--- src/proxy/server.ts
+++ src/proxy/server.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
+import * as request from 'request';
 
 namespace express_web_api {
   let app = express();
@@ -7,12 +8,39 @@ namespace express_web_api {
 
   app.get('/', (req: Request, resp: Response) => {
     proxyToSfdc();
-    resp.send('{"msg": "Hello Express!!"}');
+    resp.send('{"msg": "Hello Express!!"}\n');
   });
 
   app.listen(port, () => console.log(`Express app listening on port ${port}`));
 
   function proxyToSfdc() {
     console.log('connect to salesforce');
+
+    // let params = {
+    //   url: 'https://login.salesforce.com/services/oauth2/token',
+    //   method: 'POST',
+    //   json: true,
+    //   form: {
+    //     grant_type: 'password',
+    //     client_id: 'xxxxxxxxxxxxxxxxxxx',
+    //     client_secret: 'nnnnnnnnnnnnnnnnnnnnn',
+    //     username: 'xxx@xxx.xxx.com',
+    //     password: 'xxxxxxxxxx'
+    //   }
+    // };
+
+    let acc_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+    let params = {
+      url: 'https://instance.salesforce.com/services/data/v38.0/sobjects',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + acc_token
+      }
+    };
+
+    request(params, (err, res, body) => {
+      console.log(Object.keys(JSON.parse(body)));
+    });
   }
 }
